18a44fb2c77f5f4cc788f0f86bb8e4e8a1fb1866
Hariii <harinig1818@gmail.com>
2026-08-22 12:30:51 +0530

    Time: 312 ms (62.85%), Space: 18.8 MB (89.21%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -0,0 +1,79 @@
+
+class TrieNode {
+public:
+    string word;
+    TrieNode* chars[26];
+
+    TrieNode() {
+        word = "";
+        for (int i = 0; i < 26; i++) {
+            chars[i] = nullptr;
+        }
+    }
+};
+
+class Solution {
+private:
+    int diff[5] = {0, 1, 0, -1, 0};
+
+    void buildTrie(TrieNode* root, const string& word) {
+        TrieNode* curr = root;
+        for (char ch : word) {
+            int idx = ch - 'a';
+            if (curr->chars[idx] == nullptr) {
+                curr->chars[idx] = new TrieNode();
+            }
+            curr = curr->chars[idx];
+        }
+        curr->word = word;
+    }
+
+    void dfs(int R, int C, int row, int col, vector<vector<char>>& board, TrieNode* curr, vector<string>& ans) {
+        char ch = board[row][col];
+        int chidx = ch - 'a';
+
+        if (ch == '#' || curr->chars[chidx] == nullptr) {
+            return;
+        }
+
+        curr = curr->chars[chidx];
+
+        if (!curr->word.empty()) {
+            ans.push_back(curr->word);
+            curr->word = "";
+        }
+
+        board[row][col] = '#';
+
+        for (int i = 0; i < 4; i++) {
+            int ar = row + diff[i];
+            int ac = col + diff[i + 1];
+            if (ar >= 0 && ac >= 0 && ar < R && ac < C && board[ar][ac] != '#') {
+                dfs(R, C, ar, ac, board, curr, ans);
+            }
+        }
+
+        board[row][col] = ch;
+    }
+
+public:
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+        TrieNode* root = new TrieNode();
+
+        for (const string& word : words) {
+            buildTrie(root, word);
+        }
+
+        vector<string> ans;
+        int R = board.size();
+        int C = board[0].size();
+
+        for (int row = 0; row < R; row++) {
+            for (int col = 0; col < C; col++) {
+                dfs(R, C, row, col, board, root, ans);
+            }
+        }
+
+        return ans;
+    }
+};
